152bf9b77656a915d26846fe7706a8d7f79f89b8
Anna Atiskova <anna.atiskova@gmail.com>
2023-03-16 20:04:36 +0200

    index

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter  
-    // basename='/case-task/'
+    basename='/case-task/'
     >
       <Provider store={store}>
         <App />
